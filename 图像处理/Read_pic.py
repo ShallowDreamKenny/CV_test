477f4c64dd406e0207dd5f4fc5531a878839e6d0
Kenny <kenny0326@qq.com>
2022-10-12 18:38:43 +0800

    图像读取

---
+++ 图像处理/Read_pic.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# coding:utf-8
+"""
+# File       : Read_pic.py
+# Time       ：10/12/22 6:34 PM
+# Author     ：Kust Kenny
+# version    ：python 3.6
+# Description：
+"""
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
