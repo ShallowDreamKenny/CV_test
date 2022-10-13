6745ab1f9e3c4c2ecce11bf540aa1b8bdbf34911
Kenny <kenny0326@qq.com>
2022-10-13 21:52:57 +0800

    图像轮廓

---
+++ 图像处理/图像轮廓/pic_ContoursDetect.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# coding:utf-8
+"""
+# File       : pic_ContoursDetect.py
+# Time       ：10/13/22 9:33 PM
+# Author     ：Kust Kenny
+# version    ：python 3.6
+# Description：
+"""
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+"""
+cv2.findContours(img,mode,method)
+
+mode: 轮廓检索模式
+    1. RETR_EXTERNAL :  只检测最外面的轮廓
+    2. RETR_LIST: 检索所有的轮廓，并将其保存到一条链表当中；
+    3  RETR_CCOMP: 检索所有轮廓，并将它们组织为两层；，顶层十个部分的外部边界，第二层是空洞的边界
+    4. RETR_TREE:  检索所有的轮廓，并重构嵌套轮廓的整个层次  最常用！！！！！！
+
+method： 轮廓逼近方法（常用的两种）
+    1. CHAIN_APPROX_NONE: 以Freeman编码的方式输出轮廓，所有其他方法输出多边形（顶点的序列）。
+    2. CHAIN_APPROX_SIMPLE: 压缩水平的、、垂直的和斜的部分，也就是函数值保留他们的重点部分
+"""
+def cv_show(img,name):
+    cv2.imshow(name,img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+img = cv2.imread("pic/car.jpg")
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+ret, thresh = cv2.threshold(gray,127,255,cv2.THRESH_BINARY)
+cv_show(thresh,"thresh")
+
+# binary, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
+contours,hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
+"""
+binary: 二值化后的图像  （新版本不再返回binary）
+contours： 返回的所有轮廓点
+hierarchy： 
+"""
+draw_img = img.copy()
+#-1为所有轮廓，0为
+res = cv2.drawContours(draw_img,contours,-1,(0,0,255),2)
+res_2 = cv2.drawContours(draw_img,contours,0,(0,0,255),2)
+res = np.hstack((res,res_2))
+cv_show(res,"res")
