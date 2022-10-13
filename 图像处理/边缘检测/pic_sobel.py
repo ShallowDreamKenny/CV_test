64ca026be8696b97e47a49c3e67def5668939b98
Kenny <kenny0326@qq.com>
2022-10-13 18:26:40 +0800

    边缘检测

---
+++ 图像处理/边缘检测/pic_sobel.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# coding:utf-8
+"""
+# File       : pic_sobel.py
+# Time       ：10/13/22 5:01 PM
+# Author     ：Kust Kenny
+# version    ：python 3.6
+# Description：
+"""
+import cv2
+
+img = cv2.imread("pic.png")
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
